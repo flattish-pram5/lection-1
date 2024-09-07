14db79d11a3905ecb7889b2e678674fbf12802fe
eti-ihor-n <eti.ihor.n@gmail.com>
2024-09-07 21:51:03 +0300

    Create 2-tasks.js

---
+++ 2-tasks.js
@@ -0,0 +1,97 @@
+// TASK 1
+// Напишіть програму, яка отримує від користувача 2 числа.
+// Вивести різницю з допомогою функції alert.
+// Підказка: для отримання чисел від користувача використовуйте функцію prompt
+
+function getDifference() {
+  // your code here
+}
+
+getDifference();
+
+// TASK 2
+// Напишіть програму, яка отримує від користувача 2 числа.
+// Вивести максимальне з цих двох чисел.
+// Підказка: для отримання чисел від користувача використовуйте функцію prompt
+
+function getMax() {
+  // your code here
+}
+
+getMax();
+
+// TASK 3
+// Напишіть програму, яка отримує від користувача 2 рядки і повертає їх конкатенацію.
+// Підказка: для отримання рядків від користувача використовуйте функцію prompt
+
+function concatenateStrings() {
+  // your code here
+}
+
+concatenateStrings();
+
+// TASK 4
+// Напишіть функцію, яка отримує рядок і виводить його довжину.
+// Підказка: для отримання рядка від користувача використовуйте функцію prompt
+
+function showLength() {
+  // your code here
+}
+
+showLength();
+
+// TASK 5
+// Напишіть функцію, яка конвертує введений рядок у верхній регістр.
+// Підказка: для отримання рядка від користувача використовуйте функцію prompt
+
+function toUpperCase() {
+  // your code here
+}
+
+toUpperCase();
+
+// TASK 6
+// Напишіть функцію, яка реверсує заданий рядок.
+// Підказка: для отримання рядка від користувача використовуйте функцію prompt
+
+function reverseString() {
+  // your code here
+}
+
+reverseString();
+
+// TASK 7 *HARD*
+// Напишіть програму, яка отримує від користувача 3-цифрове число і обертає його.
+// Ви можете використовувати лише числа та їх оператори.
+// Не використовуйте тут строки!
+
+function reverseNumber() {
+  // your code here
+}
+
+reverseNumber();
+
+// TASK 8 *HARD*
+// Напишіть функцію, яка отримує на вхід речення і повертає найдовше слово у цьому реченні.
+// Якщо існує декілька слів однакової довжини, повернути перше з них.
+
+function findLongestWord(someString) {
+  // your code here
+}
+
+console.log(findLongestWord('I am a coding god')); // should return "coding"
+console.log(findLongestWord('I would like to work for Google')); // should return "Google"
+console.log(findLongestWord('Ain`t nobody got time for that')); // should return "nobody"
+
+// TASK 9 *HARD*
+// Напишіть функцію, яка повертає перший неповторюваний символ у рядку.
+// Якщо всі символи повторюються, повернути null.
+
+function firstNonRepeatingCharacter(someString) {
+  // your code here
+}
+
+console.log(firstNonRepeatingCharacter('hello')); // should return "h"
+console.log(firstNonRepeatingCharacter('swiss')); // should return "w"
+console.log(firstNonRepeatingCharacter('aabbcc')); // should return null
+console.log(firstNonRepeatingCharacter('ccdd')); // should return null
