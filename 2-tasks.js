4e4e58ed218e0c07cf2bed2ee24004552a29fd3e
flattish-pram5 <vadim040209@gmail.com>
2024-09-11 17:32:36 +0300

    Update 2-tasks.js

--- 2-tasks.js
+++ 2-tasks.js
@@ -4,7 +4,10 @@
 // Підказка: для отримання чисел від користувача використовуйте функцію prompt
 
 function getDifference() {
-  // your code here
+const num1 = prompt("Введіть перше число:");
+const num2 = prompt("Введіть друге число:");
+let d = num1 - num2;
+alert("Різниця між числами: " + d);
 }
 
 getDifference();
@@ -14,84 +17,17 @@ getDifference();
 // Вивести максимальне з цих двох чисел.
 // Підказка: для отримання чисел від користувача використовуйте функцію prompt
 
-function getMax() {
-  // your code here
+function getMax () {
+    const num1 = prompt("Введіть перше число:");
+    const num2 = prompt("Введіть друге число:");
+    if (num1 > num2) {
+        alert("Перше число більше: " + num1);
+    } else {
+        alert("Друге число більше: " + num2);
+    }
 }
-
 getMax();
 
 // TASK 3
-// Напишіть програму, яка отримує від користувача 2 рядки і повертає їх конкатенацію.
-// Підказка: для отримання рядків від користувача використовуйте функцію prompt
-
-function concatenateStrings() {
-  // your code here
-}
-
-concatenateStrings();
-
-// TASK 4
-// Напишіть функцію, яка отримує рядок і виводить його довжину.
-// Підказка: для отримання рядка від користувача використовуйте функцію prompt
-
-function showLength() {
-  // your code here
-}
-
-showLength();
-
-// TASK 5
-// Напишіть функцію, яка конвертує введений рядок у верхній регістр.
-// Підказка: для отримання рядка від користувача використовуйте функцію prompt
-
-function toUpperCase() {
-  // your code here
-}
-
-toUpperCase();
-
-// TASK 6
-// Напишіть функцію, яка реверсує заданий рядок.
-// Підказка: для отримання рядка від користувача використовуйте функцію prompt
-
-function reverseString() {
-  // your code here
-}
-
-reverseString();
-
-// TASK 7 *HARD*
-// Напишіть програму, яка отримує від користувача 3-цифрове число і обертає його.
-// Ви можете використовувати лише числа та їх оператори.
-// Не використовуйте тут строки!
-
-function reverseNumber() {
-  // your code here
-}
-
-reverseNumber();
-
-// TASK 8 *HARD*
-// Напишіть функцію, яка отримує на вхід речення і повертає найдовше слово у цьому реченні.
-// Якщо існує декілька слів однакової довжини, повернути перше з них.
-
-function findLongestWord(someString) {
-  // your code here
-}
-
-console.log(findLongestWord('I am a coding god')); // should return "coding"
-console.log(findLongestWord('I would like to work for Google')); // should return "Google"
-console.log(findLongestWord('Ain`t nobody got time for that')); // should return "nobody"
-
-// TASK 9 *HARD*
-// Напишіть функцію, яка повертає перший неповторюваний символ у рядку.
-// Якщо всі символи повторюються, повернути null.
-
-function firstNonRepeatingCharacter(someString) {
-  // your code here
-}
-
-console.log(firstNonRepeatingCharacter('hello')); // should return "h"
-console.log(firstNonRepeatingCharacter('swiss')); // should return "w"
-console.log(firstNonRepeatingCharacter('aabbcc')); // should return null
-console.log(firstNonRepeatingCharacter('ccdd')); // should return null
+// Завдання: вивести в консолі «Hello World» або за допомогою функції alert, якщо ви використовуєте браузер
+// your code here: alert("hello world");
